404181d2b4d6a8007d79d58940b63b24c48d32be
Maneth Pirisyala <mpirisyala2@gmail.com>
2022-01-30 12:33:23 +0530

    firt3

--- users/views.py
+++ users/views.py
@@ -9,8 +9,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created for {username}')
-            return redirect('projects')
+            messages.success(request, f'Account Created for {username}. Please Log In')
+            return redirect('login')
 
     else:
         form = UserRegisterForm()
