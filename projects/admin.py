bad18f58c5e0b4ade4aed08737ff83b86ebe72be
Maneth Pirisyala <mpirisyala2@gmail.com>
2022-01-31 20:21:46 +0530

    firt3

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, projectAccounts
 
 admin.site.register(Project)
-# Register your models here.
+admin.site.register(projectAccounts)
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-import random
-import uuid
+from urllib.request import urlopen
+import json
 
+response = urlopen("https://www.instagram.com/web/search/topsearch/?context=blended&query=instagram")
 
-print(id)
+with open('read2.json', 'w') as f:
+    json.dump(json.loads(response.read()), f, indent=4)
